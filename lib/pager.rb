be7abb986a81b4fc42e21d387efe3acd59b3f6eb
Rodrigo Álvarez <papipo@gmail.com>
2023-05-09 14:01:20 +0200

    Add naive alert feature.

---
+++ lib/pager.rb
@@ -0,0 +1,20 @@
+class Pager
+  ACK_TIMEOUT = 60 * 15
+  attr_reader :timer, :escalation, :repo, :notifier
+
+  def initialize(timer:, escalation:, repo:, notifier:)
+    @timer = timer
+    @escalation = escalation
+    @repo = repo
+    @notifier = notifier
+  end
+
+  def alert(service_id:, message:)
+    level = repo.unhealthy(service_id)
+    escalation.recipients(service_id: service_id, level: level).each do |recipient|
+      notifier.call(recipient: recipient, message: message)
+    end
+    timer.set(id: service_id, seconds: ACK_TIMEOUT)
+  end
+
+end
---
+++ spec/pager_spec.rb
@@ -0,0 +1,35 @@
+require "pager"
+
+RSpec.describe Pager do
+  let(:timer) { double("Timer service client", set: true) }
+  let(:escalation) { double("Escalation service client") }
+  let(:notifier) { double("Notifier service client", call: true) }
+  let(:repo) { double("Pager persitence", unhealthy: 1) }
+  let(:service_id) { "some-fake-id" }
+  let(:message) { "This message describes the issue" }
+  let(:recipient) { double("This represents a transport [SMS | Email] + address/phone tuple") }
+
+  let(:pager) do
+    Pager.new(timer: timer, escalation: escalation, repo: repo, notifier: notifier)
+  end
+
+  describe "alert on Healthy service" do
+    before do
+      allow(escalation).to receive(:recipients).with(service_id: service_id, level: 1).and_return([recipient])
+    end
+
+    after { pager.alert(service_id: service_id, message: message) }
+
+    it "switches the service to Unhealthy" do
+      expect(repo).to receive(:unhealthy).with(service_id).and_return(1)
+    end
+
+    it "notifies all targets of the first level of the escalation policy" do
+      expect(notifier).to receive(:call).once.with({recipient: recipient, message: message})
+    end
+
+    it "sets a 15-minutes acknowledgement delay" do
+      expect(timer).to receive(:set).with({id: service_id, seconds: 60 * 15})
+    end
+  end
+end
